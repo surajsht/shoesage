5009c9d030f264d274ece795d382d39b6177bded
suraj <surajsht2055@gmail.com>
2024-05-19 20:10:19 +0545

    routing and hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./page/Home";
+import Shop from "./page/Shop";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <img src="/hero.webp" alt="hero-image" />
+
+      <div className="hero-content-container">
+        <div className="wrapper">
+          <div className="column-row">
+            <div className="column column-9 column-md-12">
+              <div className="hero-content">
+                <h6 className="hero-sub-title">Discover Your Perfect Fit</h6>
+                <h2 className="hero-title">
+                  Explore Our Range of Stylish and Sustainable Fashion
+                  Essentials
+                </h2>
+                <Link to={"/shop"} className="primary-btn">
+                  Shop Now
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
