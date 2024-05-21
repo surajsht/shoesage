932195870aa5f7958a59df9d87873b451f728f3a
suraj <surajsht2055@gmail.com>
2024-05-21 19:41:25 +0545

    fixed loader css issue

--- src/components/loader/productsLoader/ProductsLoader.jsx
+++ src/components/loader/productsLoader/ProductsLoader.jsx
@@ -3,35 +3,39 @@ import "./productsLoader.css";
 const ProductsLoader = () => {
   return (
     <div className="product-loader-container">
-      <div className="loader-item">
-        <div className="loader-image"></div>
-        <div className="loader-title"></div>
-        <div className="loader-price"></div>
-      </div>
-      <div className="loader-item">
-        <div className="loader-image"></div>
-        <div className="loader-title"></div>
-        <div className="loader-price"></div>
-      </div>
-      <div className="loader-item">
-        <div className="loader-image"></div>
-        <div className="loader-title"></div>
-        <div className="loader-price"></div>
-      </div>
-      <div className="loader-item">
-        <div className="loader-image"></div>
-        <div className="loader-title"></div>
-        <div className="loader-price"></div>
-      </div>
-      <div className="loader-item">
-        <div className="loader-image"></div>
-        <div className="loader-title"></div>
-        <div className="loader-price"></div>
-      </div>
-      <div className="loader-item">
-        <div className="loader-image"></div>
-        <div className="loader-title"></div>
-        <div className="loader-price"></div>
+      <div className="wrapper">
+        <div className="product-loader-content">
+          <div className="loader-item">
+            <div className="loader-image"></div>
+            <div className="loader-title"></div>
+            <div className="loader-price"></div>
+          </div>
+          <div className="loader-item">
+            <div className="loader-image"></div>
+            <div className="loader-title"></div>
+            <div className="loader-price"></div>
+          </div>
+          <div className="loader-item">
+            <div className="loader-image"></div>
+            <div className="loader-title"></div>
+            <div className="loader-price"></div>
+          </div>
+          <div className="loader-item">
+            <div className="loader-image"></div>
+            <div className="loader-title"></div>
+            <div className="loader-price"></div>
+          </div>
+          <div className="loader-item">
+            <div className="loader-image"></div>
+            <div className="loader-title"></div>
+            <div className="loader-price"></div>
+          </div>
+          <div className="loader-item">
+            <div className="loader-image"></div>
+            <div className="loader-title"></div>
+            <div className="loader-price"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
