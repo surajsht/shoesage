d2f20b903dcbf72e417d57b33a3f32e6875a083f
suraj <surajsht2055@gmail.com>
2024-05-23 20:30:44 +0545

    navbar design complete

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,48 @@
+import { useRef, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { FaHeart, FaShoppingCart } from "react-icons/fa";
 import { HiBars3BottomLeft } from "react-icons/hi2";
+import { GrFormClose } from "react-icons/gr";
 import "./navbar.css";
 
 const Navbar = () => {
+  let [offcanvas, setOffcanvas] = useState(false);
+  let navLinkRefContainer = useRef(null);
+  let navLinkRef = useRef(null);
+
+  let openOffcanvasMenu = () => {
+    let navLinkHeight = navLinkRef.current.offsetHeight;
+    navLinkRefContainer.current.style.height = navLinkHeight + "px";
+    navLinkRefContainer.current.style.boxShadow =
+      "0 3px 14px 3px rgba(0, 0, 0, 0.14)";
+    setOffcanvas(true);
+  };
+
+  let closeOffcanvasMenu = () => {
+    navLinkRefContainer.current.style.height = 0;
+    navLinkRefContainer.current.style.boxShadow = "none";
+    setOffcanvas(false);
+  };
+
   return (
     <nav>
       <div className="wrapper">
         <div className="nav-content">
-          <HiBars3BottomLeft />
+          {offcanvas ? (
+            <GrFormClose onClick={() => closeOffcanvasMenu()} />
+          ) : (
+            <HiBars3BottomLeft onClick={() => openOffcanvasMenu()} />
+          )}
 
           <div className="logo">
             <h1>
-              <Link to="/"> Home </Link>
+              <Link to="/"> ShopEase </Link>
             </h1>
           </div>
 
           <div className="nav-items-container">
-            <div className="nav-links-container">
-              <ul className="nav-links">
+            <div className="nav-links-container" ref={navLinkRefContainer}>
+              <ul className="nav-links" ref={navLinkRef}>
                 <li>
                   <NavLink to="/"> Home </NavLink>
                 </li>
