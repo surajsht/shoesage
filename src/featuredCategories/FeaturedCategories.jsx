c472b5ce5c4244f9896f154a972c822fe48be4bf
suraj <surajsht2055@gmail.com>
2024-06-25 22:32:19 +0545

    new section added

--- src/featuredCategories/FeaturedCategories.jsx
+++ src/featuredCategories/FeaturedCategories.jsx
@@ -1,5 +1,35 @@
+import { Link } from "react-router-dom";
+import { FeaturedCategoriesData } from "../Data";
+import "./featuredCategories.css";
+
 const FeaturedCategories = () => {
-  return <div>FeaturedCategories</div>;
+  return (
+    <div className="featured-categories-container">
+      {FeaturedCategoriesData.map((item) => {
+        let { id, title, desc, btnTxt, img, subTitle } = item;
+
+        return (
+          <div key={id} className="featured-categories-item">
+            <img src={img} alt={title} className="featured-categories-img" />
+
+            <div className="featured-categories-details">
+              {subTitle && (
+                <span className="featured-categories-sub-title">
+                  {subTitle}
+                </span>
+              )}
+
+              <h2 className="featured-categories-title"> {title} </h2>
+              <p className="featured-categories-desc">{desc}</p>
+              <Link to="/shop" className="secondary-btn outlined">
+                {btnTxt}
+              </Link>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default FeaturedCategories;
