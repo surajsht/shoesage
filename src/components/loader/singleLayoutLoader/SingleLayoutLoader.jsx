12bfca77704ed7dc2e30c2bc66338458cc2c940d
suraj <surajsht2055@gmail.com>
2024-06-28 19:48:25 +0545

    loader added for product in shop page

---
+++ src/components/loader/singleLayoutLoader/SingleLayoutLoader.jsx
@@ -0,0 +1,42 @@
+import "./singleLayoutLoader.css";
+
+const SingleLayoutLoader = () => {
+  return (
+    <div className="single-layout-loader-container">
+      <div className="single-layout-loader-item">
+        <div className="single-layout-loader-img loader-animation"></div>
+
+        <div className="single-layout-loader-detail">
+          <div className="loader-animation"></div>
+          <div className="loader-animation"></div>
+          <div className="loader-animation"></div>
+          <div className="loader-animation"></div>
+        </div>
+      </div>
+
+      <div className="single-layout-loader-item">
+        <div className="single-layout-loader-img loader-animation"></div>
+
+        <div className="single-layout-loader-detail">
+          <div className="loader-animation"></div>
+          <div className="loader-animation"></div>
+          <div className="loader-animation"></div>
+          <div className="loader-animation"></div>
+        </div>
+      </div>
+
+      <div className="single-layout-loader-item">
+        <div className="single-layout-loader-img loader-animation"></div>
+
+        <div className="single-layout-loader-detail">
+          <div className="loader-animation"></div>
+          <div className="loader-animation"></div>
+          <div className="loader-animation"></div>
+          <div className="loader-animation"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleLayoutLoader;
--- src/components/shopProducts/ShopProducts.jsx
+++ src/components/shopProducts/ShopProducts.jsx
@@ -1,31 +1,40 @@
 import { Link } from "react-router-dom";
 import { useCon } from "../../context/Context";
+import SingleLayoutLoader from "../loader/singleLayoutLoader/SingleLayoutLoader";
 import "./shopProducts.css";
 
 const ShopProducts = () => {
-  let { apiData, gridOption } = useCon();
+  let { apiData, gridOption, loading } = useCon();
 
   return (
-    <div className={`shop-product-container ${gridOption}`}>
-      {apiData.map((item) => {
-        let { id, category, description, img, price, title } = item;
+    <>
+      {loading ? (
+        <SingleLayoutLoader />
+      ) : (
+        <div className={`shop-product-container ${gridOption}`}>
+          {apiData.map((item) => {
+            let { id, category, description, img, price, title } = item;
 
-        return (
-          <div key={id} className="shop-product-item">
-            <img src={img} alt="product-item" />
+            return (
+              <div key={id} className="shop-product-item">
+                <img src={img} alt="product-item" />
 
-            <div className="product-detail">
-              <h2 className="shop-product-title">
-                <Link to={`/product/${id}`}> {title} </Link>
-              </h2>
-              <span className="shop-product-category"> {category} </span>
-              <p className="line-clamp line-clamp-5"> {description} </p>
-              <span className="product-price"> Rs.{price} </span>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+                <div className="product-detail">
+                  <h2 className="shop-product-title">
+                    <Link to={`/product/${id}`}> {title} </Link>
+                  </h2>
+                  <span className="shop-product-category">
+                    Category: {category}
+                  </span>
+                  <p className="line-clamp line-clamp-5"> {description} </p>
+                  <span className="product-price"> Rs.{price} </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
 
